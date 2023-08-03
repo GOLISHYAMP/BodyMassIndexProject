6f47c1f206fcae99638e0c2f907f5df1d9ad9ad0
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-03 22:25:19 +0530

    almost done

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    render_template("index.html")
+        return render_template("index.html")
+   
 
-@app.route('/result',methods = ["get", "post"])
-def result():
-    if request.method == "post":
-        render_template('result.html')
+@app.route('/predict',methods = ["GET", "POST"])
+def predict():  
+    if request.method == "POST":
+        weight = int(request.form['weight'])
+        height = int(request.form['height'])
+        result = weight * height
+        return render_template('predict.html', show = result)
     else:
-        redirect(url_for("index"))
+        return redirect(url_for("index"))
+
+@app.route('/result')
+def result():
+     return render_template("result.html", show = "please enter the correct credentials")
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)    
